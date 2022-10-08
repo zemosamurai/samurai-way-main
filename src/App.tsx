43633576cb7839f6fb4c4b41d8d50fe2a20befe3
zemosamurai <zemosamurai@gmail.com>
2022-10-08 18:05:26 +0300

    Change README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+     <Header/>
+     <Technologies/>
     </div>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <a href='#'>Home</a>
+            <a href='#'>Home</a>
+            <a href='#'>Home</a>
+        </div>
+    )
+}
+
+export default Header;
