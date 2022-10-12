836fd9fc3eee243faf314e34ae5cabefcc3985e3
zemosamurai <zemosamurai@gmail.com>
2022-10-12 15:58:29 +0300

    edited file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
 
 function App() {
-  return (
-    <div>
-     <Header/>
-     <Technologies/>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <Profile/>
+        </div>
+    );
 }
 
 export default App;
