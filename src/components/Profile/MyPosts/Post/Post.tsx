61f15c66cf0c59d61ecf6a3e7653602542e1808b
zemosamurai <zemosamurai@gmail.com>
2022-10-18 13:12:50 +0300

    add props for post

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import s from './Post.module.css';
 
-function Post() {
+type PostPropsType = {
+    message: string
+    likesCount: number
+}
+
+function Post(props: PostPropsType) {
     return (
         <div className={s.item}>
             <img alt=''
                  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8cCnLjdRdi8dnxEXbDxjZt7W7UZAZwqbjjw&usqp=CAU'/>
-            Post1
-            <div>Like</div>
+            {props.message}
+            <div>Like {props.likesCount}</div>
         </div>
     )
 }
