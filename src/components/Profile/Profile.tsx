b555bfcabed082432495fdf56f64e04bf74d8b9c
zemosamurai <zemosamurai@gmail.com>
2022-10-25 10:56:52 +0300

    import ProfileInfo

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import s from './Profile.module.css';
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 function Profile() {
     return (
-        <div className={s.content}>
-            <div>
-                <img alt='' src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'/>
-            </div>
-            <div>
-                ava + description
-            </div>
+        <div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     )
