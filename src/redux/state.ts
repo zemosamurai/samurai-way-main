5f35010f7acbe8f13dc6744f2f928e12127a7f61
zemosamurai <zemosamurai@gmail.com>
2022-10-27 17:04:04 +0300

    add local state (type redux)

---
+++ src/redux/state.ts
@@ -0,0 +1,26 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: 12},
+            {id: 2, message: 'It\'s my first post', likesCount: 11}
+        ]
+    },
+    dialogsPage: {
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'Im Kim'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'}
+        ],
+        dialogs: [
+            {id: 1, name: 'Sasha'},
+            {id: 2, name: 'Petya'},
+            {id: 3, name: 'Vitaliy'},
+            {id: 4, name: 'Denis'},
+            {id: 5, name: 'Victor'}
+        ]
+    }
+}
+
+export default state
