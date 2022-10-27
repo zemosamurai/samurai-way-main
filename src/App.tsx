4777b4e4c639e96e0c331881b51f34eecd3bd5ac
zemosamurai <zemosamurai@gmail.com>
2022-10-27 17:03:25 +0300

    add BrowserRouter and Route

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,53 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
-function App() {
+export type MessagesType = {
+    id: number
+    message: string
+}
+
+export type DialogsType = {
+    id: number
+    name: string
+}
+
+export type PostType = {
+    id: number
+    message: string
+    likesCount: number
+}
+
+export type profilePageType = {
+    posts: Array<PostType>
+}
+
+export type dialogsPageType = {
+    dialogs: Array<DialogsType>
+    messages: Array<MessagesType>
+}
+
+type AppPropsType = {
+    state: {
+        profilePage: profilePageType
+        dialogsPage: dialogsPageType
+    }
+}
+
+function App(props: AppPropsType) {
+    let SomeComponentProfile = () => <Profile state={props.state.profilePage}/>
+    let SomeComponentDialogs = () => <Dialogs state={props.state.dialogsPage}/>
+
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/profile' component={Profile}/>
-                    <Route path='/dialogs' component={Dialogs}/>
+                    {/*<Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>*/}
+                    <Route path='/profile' render={SomeComponentProfile}/>
+                    <Route path='/dialogs' render={SomeComponentDialogs}/>
+
                     <Route path='/news' component={News}/>
                     <Route path='/music' component={Music}/>
                     <Route path='/settings' component={Settings}/>
