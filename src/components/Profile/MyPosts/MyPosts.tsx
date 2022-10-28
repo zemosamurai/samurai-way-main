acbb87b7c944072e082c7fb149f0f561c964f324
zemosamurai <zemosamurai@gmail.com>
2022-10-28 12:33:09 +0300

    refactoring is code and import

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
+import {profilePageType} from "../../../App";
 
-function MyPosts() {
+
+function MyPosts(props: profilePageType) {
+
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+
     return (
         <div className={s.postsBlock}>
             <h3>My post</h3>
@@ -16,8 +21,7 @@ function MyPosts() {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={'Hi, how are you?'} likesCount={15}/>
-                <Post message={"It's my first post"} likesCount={20}/>
+                {postsElements}
             </div>
         </div>
     )
