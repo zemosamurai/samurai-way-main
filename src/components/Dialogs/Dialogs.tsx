51b8661e5293fa8cfda5362e325be5da2f63fe2c
zemosamurai <zemosamurai@gmail.com>
2022-10-28 12:31:58 +0300

    refactoring is code

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,47 +1,27 @@
 import React from 'react';
 import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
+import {dialogsPageType} from "../../App";
 
-type DialogItemPropsType = {
-    name: string
-    id: number
+type DialogsPropsType = {
+    state: dialogsPageType
 }
 
-const DialogItem = (props: DialogItemPropsType) => {
-    return (
-        <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
-        </div>
-    )
-}
+const Dialogs = (props: DialogsPropsType) => {
+    let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
 
-type MessagePropsType = {
-    message: string
-}
-
-const Message = (props: MessagePropsType) => {
-    return (
-        <div className={s.message}>{props.message}</div>
-    )
-}
-
-const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={'Sasha'} id={1}/>
-                <DialogItem name={'Petya'} id={2}/>
-                <DialogItem name={'Vitaliy'} id={3}/>
-                <DialogItem name={'Masha'} id={4}/>
-                <DialogItem name={'Victor'} id={5}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={'Hi'}/>
-                <Message message={'Im Kim'}/>
-                <Message message={'Yo'}/>
+                {messagesElements}
             </div>
         </div>
     )
 }
 
-export default Dialogs
+export default Dialogs;
