90edb12e1714936300415be0b67c5690537ca256
zemosamurai <zemosamurai@gmail.com>
2022-11-18 18:59:37 +0300

    use props.dispatch

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
-import {dialogsPageType} from "../../redux/state";
+import {ActionsType, dialogsPageType} from "../../redux/state";
+import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogsReducer";
 
 type DialogsPropsType = {
     state: dialogsPageType
+    dispatch: (action: ActionsType) => void
 }
 
 const Dialogs = (props: DialogsPropsType) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
     let messagesElements = props.state.messages.map(m => <Message message={m.message}/>)
+    let newMessagesBody = props.state.newMessageBody
 
+    const onSendMessageClick = () => {
+        props.dispatch(sendMessageCreator())
+    }
+    const onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        let body = e.currentTarget.value
+        props.dispatch(updateNewMessageBodyCreator(body))
+    }
 
     return (
         <div className={s.dialogs}>
@@ -19,8 +29,15 @@ const Dialogs = (props: DialogsPropsType) => {
                 {dialogsElements}
             </div>
             <div className={s.messages}>
-                {messagesElements}
+                <div>{messagesElements}</div>
+                <div>
+                    <div><textarea value={newMessagesBody}
+                                   onChange={onNewMessageChange}
+                                   placeholder={'Enter your message'}></textarea></div>
+                    <div><button onClick={onSendMessageClick}>Send</button></div>
+                </div>
             </div>
+
         </div>
     )
 }
