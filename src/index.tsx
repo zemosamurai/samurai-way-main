7e9be29ff65571376bfbadb6f9b6f3ad50757389
zemosamurai <zemosamurai@gmail.com>
2022-11-24 17:16:29 +0300

    transfer store from app

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import store from "./redux/state";
+import store from "./redux/redux-store";
+import {RootStateType} from "./redux/state";
 
 
-let rerenderEntireThree = () => {
+let rerenderEntireThree = (state: RootStateType) => {
     ReactDOM.render(
         <BrowserRouter>
             <App
-                state={store.getState()}
+                state={state}
                 dispatch={store.dispatch.bind(store)}
+                store={store}
             />
         </BrowserRouter>, document.getElementById('root')
     );
 }
 
-rerenderEntireThree()
-store.subscribe(rerenderEntireThree)
+rerenderEntireThree(store.getState())
+store.subscribe(() => {
+    let state = store.getState()
+    rerenderEntireThree(state)
+})
