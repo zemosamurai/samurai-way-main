bb4df26d4c06eac0f48d6c843b35b2cf650ac97c
zemosamurai <zemosamurai@gmail.com>
2022-11-24 17:16:59 +0300

    add dialogsReducer.ts

--- src/redux/dialogsReducer.ts
+++ src/redux/dialogsReducer.ts
@@ -1,12 +1,27 @@
-import {ActionsType, dialogsPageType,} from "./state";
+import {ActionsType} from "./state";
 
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
 const SEND_MESSAGE = 'SEND-MESSAGE'
 
-export type UpdateNewMessageBody = ReturnType<typeof updateNewMessageBodyCreator>
-export type SendMessage = ReturnType<typeof sendMessageCreator>
+const initialState = {
+    messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Im Kim'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'}
+    ],
+    dialogs: [
+        {id: 1, name: 'Sasha'},
+        {id: 2, name: 'Petya'},
+        {id: 3, name: 'Vitaliy'},
+        {id: 4, name: 'Denis'},
+        {id: 5, name: 'Victor'}
+    ],
+    newMessageBody: ''
+}
 
-const dialogsReducer = (state: dialogsPageType, action: ActionsType) => {
+const dialogsReducer = (state = initialState, action: ActionsType) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body
@@ -22,12 +37,15 @@ const dialogsReducer = (state: dialogsPageType, action: ActionsType) => {
 
 }
 
+export type UpdateNewMessageBody = ReturnType<typeof updateNewMessageBodyCreator>
 export const updateNewMessageBodyCreator = (body: string) => {
     return {
         type: 'UPDATE-NEW-MESSAGE-BODY',
         body: body
     } as const
 }
+
+export type SendMessage = ReturnType<typeof sendMessageCreator>
 export const sendMessageCreator = () => {
     return {
         type: 'SEND-MESSAGE',
