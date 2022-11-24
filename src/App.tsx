ec63eaf5ccd83a3dfa67b82c5fe1371def95b94c
zemosamurai <zemosamurai@gmail.com>
2022-11-24 17:16:06 +0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
 import {Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {ActionsType, RootStateType} from "./redux/state";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 
 export type AppPropsType = {
     state: RootStateType
     dispatch: (action: ActionsType) => void
+    store: any
 }
 
 function App(props: AppPropsType) {
