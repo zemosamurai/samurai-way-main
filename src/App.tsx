687bdb5188197a2286c4f79e2558644c763ac795
zemosamurai <zemosamurai@gmail.com>
2022-12-01 18:31:06 +0300

    new Route UserContainer

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,14 @@ import {Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {ActionsType, RootStateType} from "./redux/state";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import {UsersContainer} from "./components/Users/UsersContainer";
 
 
-export type AppPropsType = {
-    state: RootStateType
-    dispatch: (action: ActionsType) => void
-    store: any
-}
 
-function App(props: AppPropsType) {
-    let SomeComponentProfile = () => <Profile
-        // profilePage={props.state.profilePage}
-        // dispatch={props.dispatch}
-        store={props.store}
-    />
-    let SomeComponentDialogs = () => <DialogsContainer
-        store={props.store}
-        // state={props.state.dialogsPage}
-        // dispatch={props.dispatch}
-    />
+function App() {
+    let SomeComponentProfile = () => <Profile/>
+    let SomeComponentDialogs = () => <DialogsContainer/>
 
     return (
 
@@ -37,6 +24,8 @@ function App(props: AppPropsType) {
             <div className='app-wrapper-content'>
                 <Route path='/profile' render={SomeComponentProfile}/>
                 <Route path='/dialogs' render={SomeComponentDialogs}/>
+                <Route path='/users' render={() => <UsersContainer/>}/>
+
 
                 <Route path='/news' component={News}/>
                 <Route path='/music' component={Music}/>
