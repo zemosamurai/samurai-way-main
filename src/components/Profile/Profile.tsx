53e6349905388d1cd9930fc2651a96241fa606ba
zemosamurai <zemosamurai@gmail.com>
2022-12-04 14:48:26 +0300

    clear code

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,26 +1,13 @@
 import React from "react";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {StoreType} from "../../redux/state";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
 
-type ProfilePropsType = {
-    store: StoreType
-    // profilePage: profilePageType
-    // dispatch: (action: ActionsType) => void
-}
-
-function Profile(props: ProfilePropsType) {
-
+function Profile() {
     return (
         <div>
             <ProfileInfo/>
-            <MyPostsContainer
-                store={props.store}
-                // posts={props.profilePage.posts}
-                // dispatch={props.dispatch}
-                // newPostText={props.profilePage.newPostText}
-            />
+            <MyPostsContainer/>
         </div>
     )
 }
