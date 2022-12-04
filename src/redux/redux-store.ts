b22a3f8881c4f9935bcb1a76fb6bf022ed03a556
zemosamurai <zemosamurai@gmail.com>
2022-12-04 14:48:03 +0300

    add combineReducers - usersPage

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
+import {UsersReducer} from "./users-reducer";
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
-    dialogsPage: dialogsReducer
+    dialogsPage: dialogsReducer,
+    usersPage: UsersReducer
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
