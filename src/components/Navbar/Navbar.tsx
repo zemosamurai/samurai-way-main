a8b128931b285e0c61d3885ee8f15256a87d0bd7
zemosamurai <zemosamurai@gmail.com>
2022-12-05 20:29:03 +0300

    new NavLink /users

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,7 +11,11 @@ function Navbar() {
                 <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink></div>
+                <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to='/users' activeClassName={s.activeLink}>Users</NavLink>
+            </div>
             <div className={s.item}>
                 <NavLink to='/news' activeClassName={s.activeLink}>News</NavLink>
             </div>
