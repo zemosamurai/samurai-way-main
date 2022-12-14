ba02bfb7e102ea40e30e733c8e2698bf3ff5011e
zemosamurai <zemosamurai@gmail.com>
2022-12-14 16:11:57 +0300

    add in mapDispatchToProps (setCurrentPage & setTotalUsersCount )

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import {connect} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
 import {Dispatch} from 'redux';
-import {followAC, InitialStateType, setUsersAC, unFollowAC, UserType} from "../../redux/users-reducer";
+import {
+    followAC,
+    InitialStateType,
+    setCurrentPageAC,
+    setUsersAC, setUsersTotalCountAC,
+    unFollowAC,
+    UserType
+} from "../../redux/users-reducer";
 import Users from "./Users";
 
 export type UsersPropsType = mapStateToPropsType & mapDispatchToPropsType
@@ -17,6 +24,8 @@ type mapDispatchToPropsType = {
     follow: (userId: number) => void
     unFollow: (userId: number) => void
     setUsers: (users: Array<UserType>) => void
+    setCurrentPage: (pageNumber: number) => void
+    setTotalUsersCount: (totalCount: number) => void
 }
 
 const mapStateToProps = (state: AppStateType): mapStateToPropsType => {
@@ -38,6 +47,12 @@ const mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsType => {
         },
         setUsers: (users: Array<UserType>) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (pageNumber: number) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount: number) => {
+            dispatch(setUsersTotalCountAC(totalCount))
         }
     }
 }
