31bbae3557c708804e7a15c7b967268c72b209b7
zemosamurai <zemosamurai@gmail.com>
2022-12-15 16:57:04 +0300

    add component Preload

---
+++ src/components/common/preloader/Preloader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import preloader from "../../../assets/images/preload.svg";
+
+export const Preloader = () => {
+    return (
+        <div>
+            <img src={preloader} alt='#'/>
+        </div>
+    );
+};
