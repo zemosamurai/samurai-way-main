c884a29e337d1a8fc63f957d70e9244468ea14fc
zemosamurai <zemosamurai@gmail.com>
2022-12-16 12:50:26 +0300

    refactoring App

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import {Route} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import {UsersContainer} from "./components/Users/UsersContainer";
-
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 
 function App() {
-    let SomeComponentProfile = () => <Profile/>
-    let SomeComponentDialogs = () => <DialogsContainer/>
-
     return (
-
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
             <div className='app-wrapper-content'>
-                <Route path='/profile' render={SomeComponentProfile}/>
-                <Route path='/dialogs' render={SomeComponentDialogs}/>
+                <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                <Route path='/dialogs' render={() => <DialogsContainer/>}/>
                 <Route path='/users' render={() => <UsersContainer/>}/>
-
 
                 <Route path='/news' component={News}/>
                 <Route path='/music' component={Music}/>
