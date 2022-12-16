e5c61962b81803bdaa9ba7aca690fdb4f71822fa
zemosamurai <zemosamurai@gmail.com>
2022-12-16 12:53:20 +0300

    add data from profile page

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import s from './ProfileInfo.module.css'
+import {Preloader} from "../../common/preloader/Preloader";
+import {MapStateType} from "../ProfileContainer";
 
-function ProfileInfo() {
+function ProfileInfo(props: MapStateType) {
+    if (!props.profile) {
+        return <Preloader/>
+    }
+
     return (
         <div>
             <div>
-                <img alt=''
+                <img alt='#'
                      src='https://lancraft.cz/Uploads/Static/Achievements/2022/2/28/b44a8835-3f19-48cb-b49f-9a58290bc15e.jpg'/>
             </div>
             <div className={s.descriptionBlock}>
+                <img src={props.profile.photos.large} alt="#"/>
+                <p>{props.profile.aboutMe}</p>
                 ava + description
             </div>
         </div>
