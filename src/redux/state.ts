d3f7bac2189e5b1a0e1f3642f1848c8a3ed2c59e
zemosamurai <zemosamurai@gmail.com>
2022-12-17 13:46:59 +0300

    ...

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -75,7 +75,7 @@ let store: StoreType = {
     },
 
     dispatch(action) {
-        this._state.profilePage = profileReducer(this._state.profilePage, action)
+        // this._state.profilePage = profileReducer(this._state.profilePage, action)
         this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action)
         this._callSubscriber()
     }
