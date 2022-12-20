f4c958c4ee3183211532326af18438f012833aac
zemosamurai <zemosamurai@gmail.com>
2022-12-20 23:05:25 +0300

    add in rootReducer new reducers

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,11 +2,13 @@ import {combineReducers, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {UsersReducer} from "./users-reducer";
+import {authReducer} from "./auth-reducer";
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
-    usersPage: UsersReducer
+    usersPage: UsersReducer,
+    auth: authReducer
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
