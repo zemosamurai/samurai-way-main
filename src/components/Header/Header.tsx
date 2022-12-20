2592dfa7bce7ffc0955104f12eb2383f9ce4e5b8
zemosamurai <zemosamurai@gmail.com>
2022-12-20 23:03:21 +0300

    add login form from Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
+import {mapStateToPropsType} from "./HeaderContainer";
 
-function Header() {
+function Header(props: mapStateToPropsType) {
     return (
         <header className={s.header}>
             <img alt='' src='https://seeklogo.com/images/C/cloud-akatsuki-logo-17E3DF7FAC-seeklogo.com.png'/>
+
+            <div className={s.loginBlock}>
+                {props.isAuth
+                    ? props.login
+                    : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     )
 }
