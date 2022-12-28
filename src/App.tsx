546ca4129f7530e2d142349ecac5024697938d5b
zemosamurai <zemosamurai@gmail.com>
2022-12-28 14:44:10 +0300

    new Route /login

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import {UsersContainer} from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import {Login} from "./components/Login/Login";
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
                 <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
                 <Route path='/dialogs' render={() => <DialogsContainer/>}/>
                 <Route path='/users' render={() => <UsersContainer/>}/>
+                <Route path='/login' render={() => <Login/>}/>
 
                 <Route path='/news' component={News}/>
                 <Route path='/music' component={Music}/>
