4a28fe38defb5c6dfc6ae8a3f3dc973f73d377a9
zemosamurai <zemosamurai@gmail.com>
2023-01-03 17:04:19 +0300

    add New class from span/input

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-export const ProfileStatus = () => {
-    return (
-        <div>
+type ProfileStatusPropsType = {
+    status: string
+}
 
-        </div>
-    );
+export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
+    state = {
+        editMode: false
+    }
+    activateEditMode()  {
+        this.setState({
+            editMode: true
+        })
+    }
+    activateViewMode() {
+        this.setState({
+            editMode: false
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                {!this.state.editMode &&
+                    <div>
+                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                    </div>
+                }
+                {this.state.editMode &&
+                    <div>
+                        <input value={this.props.status} onBlur={this.activateViewMode.bind(this)} autoFocus/>
+                    </div>
+                }
+            </div>
+        );
+    }
+
 };
